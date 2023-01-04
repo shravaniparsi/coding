5df7509b944ca25bbc318010617dc35f2dbf073f
shravani parsi <31122229+shravaniparsi@users.noreply.github.com>
2023-01-03 17:18:15 -0800

    Create 2055. Plates Between Candles.py

---
+++ 2055. Plates Between Candles.py
@@ -0,0 +1,66 @@
+class Solution:
+    def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
+        # TLE - BRUT FORCE
+#         output = []
+#         for q in queries:
+#             substr = s[q[0]:q[1]+1]
+#             # find first occurence of "|" and last occurence of "|"
+#             first_occr = substr.index('|') if '|' in substr else -1
+#             last_occr = substr[::-1].index('|') if '|' in substr else -1
+            
+#             if first_occr!=-1 and last_occr!=-1:
+#                 output.append(substr[first_occr:len(substr)-last_occr].count('*'))
+#             else:
+#                 output.append(0)
+#         return output
+        # BINARY SEARCH
+        # Ex, s = "***|**|*****|**||**|*"
+        # candles = [3,5,12,15,16,19]
+        # query = [1,17]
+        # lower_bound for 1 will be 3 (Binary search will return postion so index[postion] = 3)
+        # upper_bound for 17 will be 16(Binary search will return postion so index[postion] = 16)
+        # count = index[u_b] - index[l_b] - (u_b_indx - l_b_indx - 1) - 1
+        # count = 16 - 3 - (4 - 0 - 1 ) - 1
+        # count = 9
+        index = []
+        for i in range(len(s)):
+            if s[i] == '|':
+                index.append(i)
+        res = []
+        
+        for q in queries:
+
+            if q[1] - q[0] > 1:
+                l_b = self.binary_search(index,q[0],0)#Find lower bound (it will return same position if present else first greater)
+                u_b = self.binary_search(index,q[1],1)#Find upper bound(it will return same position if present else first smaller)
+                
+                if l_b >= u_b:
+                    res.append(0)
+                    continue
+                
+                
+                count = index[u_b] - index[l_b] - (u_b - l_b - 1) - 1
+                res.append(count)
+            else:
+                res.append(0)
+        
+        return res
+    def binary_search(self,index,val,b):
+        l = 0
+        h = len(index) - 1
+
+        while l<=h:
+            mid = (l+h)//2
+
+            if index[mid] == val:
+                return mid # If positon matches with given position
+            elif index[mid] > val:
+                h = mid - 1
+            else:
+                l = mid + 1 
+
+        if b == 0:
+            return l
+        else:
+            return h
+
